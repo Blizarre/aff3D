bc12c63d38fb8f4fed4519eb474e48787b5e330d
Simon Marache-Francisco <git@marache.net>
2018-06-09 17:40:23 +0100

    format: add hpp extension

--- format.sh
+++ format.sh
@@ -3,4 +3,6 @@
 set -e
 
 SCRIPT_DIR="$(dirname "$0")"
-find "$SCRIPT_DIR" -iname "*.cpp" -or -iname "*.h" -exec clang-format -i \{\} \;
+find "$SCRIPT_DIR" \
+  -iname "*.cpp" -or -iname "*.h" -or -iname "*.hpp" \
+  -exec clang-format -i '{}' \;
