43beed3984737aadffeac6d98882495ed429a00b
Simon Marache-Francisco <github@simon.marache.net>
2018-06-11 00:11:54 +0100

    added sdl cmake file for windows

---
+++ vstudio/sdl2-config.cmake
@@ -0,0 +1,12 @@
+# from https://stackoverflow.com/questions/28395833/using-sdl2-with-cmake?noredirect=1&lq=1
+
+set(SDL2_INCLUDE_DIR "${CMAKE_CURRENT_LIST_DIR}/include")
+
+# Support both 32 and 64 bit builds
+if (${CMAKE_SIZEOF_VOID_P} MATCHES 8)
+  set(SDL2_LIBRARIES "${CMAKE_CURRENT_LIST_DIR}/lib/x64/SDL2.lib;${CMAKE_CURRENT_LIST_DIR}/lib/x64/SDL2main.lib")
+else ()
+  set(SDL2_LIBRARIES "${CMAKE_CURRENT_LIST_DIR}/lib/x86/SDL2.lib;${CMAKE_CURRENT_LIST_DIR}/lib/x86/SDL2main.lib")
+endif ()
+
+string(STRIP "${SDL2_LIBRARIES}" SDL2_LIBRARIES)
