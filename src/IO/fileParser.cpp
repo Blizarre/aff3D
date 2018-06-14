055753603f29e9ab70d6b4789d2906ee53589e8b
Simon Marache-Francisco <github@simon.marache.net>
2018-06-14 22:16:29 +0100

    fileparser: fix reopen issues

--- src/IO/fileParser.cpp
+++ src/IO/fileParser.cpp
@@ -8,15 +8,22 @@
 #include <iostream>
 
 std::unique_ptr<FileParser> FileParser::getParser(const std::string &fileName) {
-  std::ifstream file{fileName};
-  if (!file.good()) {
-    throw ParseError(std::string("Cannot open ") + fileName + ", " +
-                     std::strerror(errno));
+  {
+    std::ifstream file{ fileName };
+    if (!file.good()) {
+      throw ParseError(std::string("Cannot open ") + fileName + ", " +
+        std::strerror(errno));
+    }
+    try {
+      return std::make_unique<ASCIISTLFile>(file);
+    }
+    catch (const InvalidFileType &e) {
+    }
   }
-  try {
-    return std::make_unique<ASCIISTLFile>(file);
-  } catch (const InvalidFileType &e) {
-  }
-  file.seekg(0, std::ios::beg);
+  // If the file couldn't be opened as an ASCIISTLFile, we try the
+  // binary version. Reseting the ifstream didn't always work,
+  // So I re-opened it instead.
+  std::ifstream file{ fileName, std::ios::binary };
+
   return std::make_unique<BINSTLFile>(file);
 }
