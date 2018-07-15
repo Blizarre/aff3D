636e23f61da9f6866f33e80a197456b9283be844
Simon Marache-Francisco <git@marache.net>
2018-07-15 17:56:12 +0100

    format: fixed script

--- format.sh
+++ format.sh
@@ -4,5 +4,5 @@ set -e
 
 SCRIPT_DIR="$(dirname "$0")"
 find "$SCRIPT_DIR" \
-  -iname "*.cpp" -or -iname "*.h" -or -iname "*.hpp" \
+  \( -iname "*.cpp" -or -iname "*.hpp" -or -iname "*.h" \) \
   -exec clang-format -i '{}' \;
